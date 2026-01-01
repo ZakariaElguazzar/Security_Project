57682d736462df5641fcd70dfd15ba7afc866f55
zakaria-el-guazzar <elguazzarzakaria10@gmail.com>
2026-01-01 18:53:21 +0100

    Creating the services

---
+++ commmande-service/src/main/java/org/elguazzar/commmandeservice/CommmandeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.elguazzar.commmandeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommmandeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommmandeServiceApplication.class, args);
+    }
+
+}
---
+++ commmande-service/src/test/java/org/elguazzar/commmandeservice/CommmandeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elguazzar.commmandeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommmandeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
